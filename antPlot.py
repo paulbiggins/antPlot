85bcc02462ae0fb5672aacc39cfc808028c2d08e
paulbiggins <paul@paulbiggins.com>
2015-02-01 01:20:29 -0800

    fix a lot of bugs in the csv writing function

--- antPlot.py
+++ antPlot.py
@@ -12,13 +12,13 @@ csv.register_dialect('semicolon', delimiter=';')
 def mathify(x,y):
     #do math and return data in different formats
 
-    if y.isdigit(): #special case for R&S formatted data
+    if y != False: #special case for R&S formatted data
         logmag = 20*math.log10(math.hypot(x,y))
     else:
         logmag = x
 
-    swr = (1+10^(logmag/20))/(1-10^(logmag/20))
-    mismatch = -10*math.log10(1-(10^(logmag/20))^2)
+    swr = (1+pow(10,(logmag/20)))/(1-pow(10,(logmag/20)))
+    mismatch = -10*math.log10(1-pow(pow(10,(logmag/20)),2))
 
     return (logmag, swr, mismatch)
 
@@ -27,9 +27,9 @@ def mathify(x,y):
 def save(name, ext):
     #generate a save file path
 
-    tempPath = os.path.dirname(os.path.realpath(sys.argv[len(sys.argv)])+'/'+name
+    tempPath = os.path.dirname(os.path.realpath(sys.argv[len(sys.argv)-1]))+'/'+name
     directory = os.path.split(tempPath)[0]
-    filename = '%s.%s' (os.path.split(tempPath)[1], ext)
+    filename = '%s.%s' % (os.path.split(tempPath)[1], ext)
     if directory == '':
         directory = '.'
     if not os.path.exists(directory):
@@ -52,24 +52,26 @@ def dataParse(f):
             datatype = 'loss'
             reader = csv.reader(f, dialect = 'semicolon')
             for row in reader:
-                if 'freq' not in row and '#' not in row:
+                if not any('freq' in s for s in row) and not any('#' in s for s in row):
                     freq = float(row[0])/1000000
                     re = float(row[1])
                     im = float(row[2])
                     (logmag, swr, mismatch) = mathify(re, im)
-                    parsedData.append(freq, logmag, swr, mismatch)
+                    rawdata = (freq, logmag, swr, mismatch)
+                    parsedData.append(rawdata)
 
 
         #the Agilent case, return loss
-        if search.find('Frequency') != -1 and search.find('Efficiency') = -1:
+        if search.find('Frequency') != -1 and search.find('Efficiency') == -1:
             datatype = 'loss'
             reader = csv.reader(f)
             for row in reader:
-                if 'Frequency' not in row and '#' not in row:
+                if not any('Frequency' in s for s in row) and not any('#' in s for s in row):
                     freq = float(row[0])/1000000
                     logmag = float(row[1])
-                    (logmag, swr, mismatch) = mathify(logmag, false)
-                    parsedData.append(freq, logmag, swr, mismatch)
+                    (logmag, swr, mismatch) = mathify(logmag, False)
+                    rawdata = (freq, logmag, swr, mismatch)
+                    parsedData.append(rawdata)
 
 
         #the Efficiency case
@@ -77,20 +79,26 @@ def dataParse(f):
             datatype = 'eff'
             reader = csv.reader(f)
             for row in reader:
-                if 'Frequency' in row and row[2].isdigit():
-                    freq = [float(i) for i in row and i.isdigit()]
-                if 'Efficiency' in row:
-                    efficiency = [float(i) for i in row and i.isdigit()]
+                if any('Frequency' in s for s in row) and any('Point' in s for s in row):
+                    row.remove('Point Values')
+                    row.remove('Frequency (MHz)')
+                    freq = [float(i) for i in row]
+                if any('Efficiency (dB)' in s for s in row):
+                    row.remove('')
+                    row.remove('Efficiency (dB)')
+                    efficiency = [float(i) for i in row]
             #split efficiency blocks
-            spacing = freq(1) - freq(0)
+            spacing = freq[1] - freq[0]
             effmap = zip(freq, efficiency)
             effblock = []
-            for i in effmap:
+            for i in range(0, len(effmap)-2):
                 if effmap[i+1][0]-effmap[i][0] == spacing:
-                    effblock.append(i)
+                    effblock.append(effmap[i])
                 else:
                     parsedData.append(effblock)
-                    effblock = [i]
+                    effblock = []
+            effblock.append(effmap[len(effmap)-1])
+            parsedData.append(effblock)
 
     finally:
         f.close()
@@ -99,7 +107,7 @@ def dataParse(f):
 
 
 
-def writeData(name, data)
+def writeData(name, data):
     #writing the data to an organized CSV
 
     sortedData = []
@@ -140,20 +148,21 @@ def writeData(name, data)
         for sublist in sortedData:
             ndx = 0
             for tup in sublist:
-                if tup.isdigit():
-                    str = ','.join(list(tup))
-                    if (len(stringList)-1 < ndx)
-                        stringList.append(str)
+                if 'eff' not in tup and 'loss' not in tup:
+                    rowstr = map(str, list(tup))
+                    if (len(stringList)-1 < ndx):
+                        stringList.append(rowstr)
                     else:
-                        stringList[ndx] += str
+                        stringList[ndx] += rowstr
                     ndx += 1
-            writer.writerow(stringList)
+        for row in stringList:
+            writer.writerow(row)
     finally:
         f.close()
 
     return
 
-def plotData(name, bandmap, data)
+#def plotData(name, bandmap, data):
     #determine number of plots needed from eff blocks
     #plot eff first
     #plot loss 100MHz from eff data edges
@@ -174,5 +183,6 @@ for i in range(2, len(sys.argv)):
         f = open(sys.argv[i], 'rt')
         data.append(dataParse(f))
 
+#tempPath = os.path.dirname(os.path.realpath(sys.argv[len(sys.argv)-1]))+'/'+name
 writeData(name, data)
-plotData(name, bandmap, data)
+#plotData(name, bandmap, data)
